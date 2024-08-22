2d450ba0efb1c88c2807f831d6461ea96dce56b8
Kenny-204 <giwatwins@gmail.com>
2024-08-22 01:54:44 +0100

    hope it works

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,116 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-// import { useState } from 'react'
+import { createContext, useContext, useState } from "react";
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import "./App.css";
 
+const weatherContext = createContext();
+
+
 function App() {
+  const [state, setState] = useState({
+    country: "",
+    temperature: "",
+    chanceOfRain: "",
+    forecast: [],
+    feelsLike: "",
+    uvIndex: "",
+    wind: "",
+    visibility: "",
+    pressure: "",
+    sunset: "",
+    humidity: "",
+    fetched:""
+  });
+  const updateState = (key, value) => {
+    setState((prevState) => ({ ...prevState, [key]: value }));
+  };
   return (
-    <div className="container">
-      <SideNav />
-      <Main />
-      <Aside />
-    </div>
+    <>
+      <weatherContext.Provider value={{ state, updateState }}>
+        <div className="container">
+          <SideNav />
+          <Main />
+          <Aside />
+        </div>
+      </weatherContext.Provider>
+    </>
+  );
+}
+
+function Main() {
+  const { state } = useContext(weatherContext);
+
+  return (
+    <main className="main">
+      <Searchbar />
+      <div className="flex" style={{justifyContent:"space-between"}}>
+
+      <div className="city">
+        <h1>{state.country || "--"} </h1>
+        <h3>Chance of rain: {state.chanceOfRain || "-"}%</h3>
+      <h1 className="temperature">{state.temperature || "--"}&deg;C</h1>
+      </div>
+      <img src="vite.svg" width="20%" />
+      </div>
+      <Forecasts />
+      <AirConditions />
+    </main>
+  );
+}
+
+function Searchbar() {
+  const { updateState } = useContext(weatherContext);
+
+  const key = "fe2cbc6eac7045b4b0f111431241708";
+  const [query, setQuery] = useState("london");
+
+  const getData = async function (country) {
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${country}&days=7&aqi=yes&alerts=no`
+      );
+      const data = await response.json();
+
+      updateState("temperature", data.current.temp_c);
+      updateState(
+        "chanceOfRain",
+        data.forecast.forecastday[0].day.daily_chance_of_rain
+      );
+      updateState("forecast", data.forecast.forecastday);
+      updateState("feelsLike", data.current.feelslike_c);
+      updateState("uvIndex", data.current.uv);
+      updateState("wind", data.current.wind_kph);
+      updateState("visibility", data.current.vis_km);
+      updateState("pressure", data.current.pressure_in);
+      updateState("sunset", data.forecast.forecastday[0].astro.sunset);
+      updateState("humidity", data.current.humidity);
+      updateState("fetched", Boolean(data))
+
+      console.log(data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          getData("country");
+          updateState("country", query);
+        }}
+      >
+        <input
+          onChange={(e) => setQuery(e.target.value[0].toUpperCase() + e.target.value.slice(1,e.target.value.length) )}
+          className="searchbar"
+          type="text"
+          placeholder="Search for cities..."
+        />
+        {/* <button className="submit">send</button> */}
+      </form>
+    </>
   );
 }
 
@@ -47,39 +146,15 @@ function SideNavItem({ name, className = "" }) {
   );
 }
 
-function Main() {
-  return (
-    <main className="main">
-      <Searchbar />
-      <div className="city">
-        <h1>Your City</h1>
-        <h3>Chance of rain: 0%</h3>
-      </div>
-      <h1 className="temperature">0 C</h1>
-      <Forecasts />
-      <AirConditions />
-    </main>
-  );
-}
-
-function Searchbar() {
-  return (
-    <>
-      <input
-        className="searchbar"
-        type="text"
-        placeholder="Search for cities..."
-      />
-    </>
-  );
-}
-
 function Forecasts() {
+  const { state } = useContext(weatherContext);
+
   return (
     <section>
       <div className="forecasts ">
         <h3>Tomorrows Forecast</h3>
         <div className="flex">
+          {/* {state.forecast[0]} */}
           <Forecast time="6:00 AM" imgsrc="#" temperature="0 C" />
           <Forecast time="6:00 AM" imgsrc="#" temperature="0 C" />
           <Forecast time="6:00 AM" imgsrc="#" temperature="0 C" />
@@ -105,19 +180,21 @@ function Forecast({ time, _, temperature }) {
 }
 
 function AirConditions() {
+  const { state } = useContext(weatherContext);
+
   return (
     <div className="forecasts ">
       <h3>Air Conditions</h3>
       <div className="flex air-cond">
-        <AirCondition title="Feels like" cap="0C" />
-        <AirCondition title="UV index" cap="3" />
-        <AirCondition title="Wind" cap="0km/h" />
-        <AirCondition title="Visisbility" cap="0km" />
+        <AirCondition title="Feels like" cap={state.feelsLike || "--"} />
+        <AirCondition title="UV index" cap={state.uvIndex || "--"} />
+        <AirCondition title="Wind" cap={state.wind + "km/h"} />
+        <AirCondition title="Visisbility" cap={state.visibility + "km"} />
       </div>
       <div className="flex air-cond">
-        <AirCondition title="Pressure" cap="0hPa" />
-        <AirCondition title="Sunset" cap="20:58" />
-        <AirCondition title="Humidity" cap="0%" />
+        <AirCondition title="Pressure" cap={state.pressure + "hPa"} />
+        <AirCondition title="Sunset" cap={state.sunset || "--:--"} />
+        <AirCondition title="Humidity" cap={state.humidity + "%"} />
       </div>
     </div>
   );
@@ -134,39 +211,47 @@ function AirCondition({ title, cap }) {
 }
 
 function Aside() {
+  const { state } = useContext(weatherContext);
+  const days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
   return (
     <aside className="aside">
       <p>7-DAY FORECAST</p>
       <ul>
-        <AsideItem
-          day="Today"
-          imgsrc="vite.svg"
-          weather="Rainy"
-          temperature="0 C"
-        />
-        <AsideItem
-          day="Today"
-          imgsrc="vite.svg"
-          weather="Rainy"
-          temperature="0 C"
-        />
-        <AsideItem
-          day="Today"
-          imgsrc="vite.svg"
-          weather="Rainy"
-          temperature="0 C"
-        />
+        {state.forecast.map((day, i) => (
+          <AsideItem
+            day={
+              i === 0
+                ? "Today"
+                : i === 1
+                ? "Tomorrow"
+                : days[new Date(day.date_epoch * 1000).getDay()]
+            }
+            imgsrc={day.day.condition.icon}
+            temperature={day.day.avgtemp_c}
+            key={i}
+          />
+        ))}
       </ul>
     </aside>
   );
 }
 
 function AsideItem({ day, imgsrc, weather, temperature }) {
+
   return (
     <>
       <li className="flex">
         <p>{day}</p>
-        <img src={imgsrc} alt="" />
+        <img src={imgsrc} alt="" width="31.88" height="32" />
         <p>{weather}</p>
         <p>{temperature}</p>
       </li>
@@ -176,3 +261,4 @@ function AsideItem({ day, imgsrc, weather, temperature }) {
 }
 
 export default App;
+
